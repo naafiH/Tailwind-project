24b620e41f5518a8a9b81f74f1bb33370f94465f
Nafih <nafithayyil@gmail.com>
2023-10-13 19:28:39 +0530

    added home

--- src/components/analytics/analytics.tsx
+++ src/components/analytics/analytics.tsx
@@ -6,12 +6,12 @@ export default function analytics() {
   return (
     <div className='w-full py-16  px-4'>
         <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
-            <img src={png2} alt="" />
-            <div>
-                <p>Data Analytic Dashboard</p>
-                <h1>Manage Data analytic centrally</h1>
+            <img className='w-[400px] mx-auto  my-4' src={png2} alt="" />
+            <div className='flex flex-col justify-center' >
+                <p className='text-blue-500 font-bold'>Data Analytic Dashboard</p>
+                <h1 className='md-text-4xl sm-text-3xl text-2xl font-bold py-2'>Manage Data analytic centrally</h1>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa excepturi eveniet illo voluptate laboriosam reprehenderit libero, non suscipit eius exercitationem soluta veritatis nulla.</p>
-                <button>Get Started</button>
+                <button className="bg-black w-[150px] rounded-md font-mono my-6 mx-auto md:mx-0 py-3 text-white">Get Started</button>
             </div>
         </div>
 
